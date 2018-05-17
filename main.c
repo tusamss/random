40c273cc9ec888e67be8655648f9c3f90fd925c2
tusamss <39378976+tusamss@users.noreply.github.com>
2018-05-17 23:22:22 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -2,37 +2,37 @@
 #include <stdlib.h>
 #include <time.h>
 
-void randum (void);
-void bubble (void);
+void Randum (void);
+void Bubble (void);
 
 int ANS[5];
 
 int main (void)
 {
 	printf("五個10~50不重複亂數:");
-	randum();
+	Randum();
 	printf("\n");
 	printf("由小到大排序後:");
-	bubble();
+	Bubble();
 	return 0;
 } 
 
-void randum (void)
+void Randum (void)
 {
-	int check=0;
+	int Check=0;
 	int i,j;
 	srand(time(NULL));
 	ANS[0]=(rand()%41)+10;
 	for( i =1;i<=4;i++)
 	{
-		check=0;
+		Check=0;
 		ANS[i]=(rand()%41)+10;
-		for(check=0;check<i;check++)
+		for(Check=0;Check<i;Check++)
 		{
-			if(ANS[i]==ANS[check])
+			if(ANS[i]==ANS[Check])
 			{
 				ANS[i]=(rand()%41)+10;
-				check=-1;
+				Check=-1;
 			}
 		}
 	}
@@ -40,18 +40,18 @@ void randum (void)
 		printf("%d ",ANS[j]);
 }
 
-void bubble (void)
+void Bubble (void)
 {
-	int temp,i,j;
+	int Temp,i,j;
 		for( i = 0; i < 5; i++) 
 	{
         for( j= i; j < 5; j++) 
 		{
             if( ANS[j] < ANS[i] ) 
 			{
-                temp = ANS[j];
+                Temp = ANS[j];
                 ANS[j] = ANS[i];
-                ANS[i] = temp;
+                ANS[i] = Temp;
             }
         }
     }
