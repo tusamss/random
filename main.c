5a2738315848e9ed0109db1b7cc3608cf965ceaf
tusamss <39378976+tusamss@users.noreply.github.com>
2018-05-18 11:34:17 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -2,37 +2,37 @@
 #include <stdlib.h>
 #include <time.h>
 
-void Randum (void);  //隨機函數產生副程式 
-void Bubble (void);	 //泡末排序副程式 
+void RandumInitial (void);  //隨機函數產生副程式 
+void BubbleSort (void);	 //泡末排序副程式 
 
 int ANS[5];
 
 int main (void)
 {
 	printf("五個10~50不重複亂數:"); //顯示提式 
-	Randum();
+	RandumInitial();
 	printf("\n");
 	printf("由小到大排序後:");  //顯示提式 
-	Bubble();
+	BubbleSort();
 	return 0;
 } 
 
-void Randum (void)//隨機函數產生副程式 
+void RandumInitial (void)//隨機函數產生副程式 
 {
-	int Check=0;  //宣告 檢查變數 
+	int repeatCheck=0;  //宣告 檢查變數 
 	int i,j;
 	srand(time(NULL));  //初始隨機函數 
 	ANS[0]=(rand()%41)+10; //設定第一格亂數 
 	for( i =1;i<=4;i++)		//不重複的for迴圈函數 
 	{
-		Check=0;			 
+		repeatCheck=0;			 
 		ANS[i]=(rand()%41)+10;//設定亂數 
-		for(Check=0;Check<i;Check++)//檢察是否跟前面變數有無重複之for迴圈 
+		for(repeatCheck=0;repeatCheck<i;repeatCheck++)//檢察是否跟前面變數有無重複之for迴圈 
 		{
-			if(ANS[i]==ANS[Check])		//當有重複時執行 
+			if(ANS[i]==ANS[repeatCheck])		//當有重複時執行 
 			{
 				ANS[i]=(rand()%41)+10; //再重取一個亂數 
-				Check=-1;//始for迴圈重新遣查一次 
+				repeatCheck=-1;//始for迴圈重新遣查一次 
 			}
 		}
 	}
@@ -40,7 +40,7 @@ void Randum (void)//隨機函數產生副程式
 		printf("%d ",ANS[j]);
 }
 
-void Bubble (void) //泡末排序副程式 
+void BubbleSort (void) //泡末排序副程式 
 {
 	int Temp,i,j;
 		for( i = 0; i < 5; i++) 
